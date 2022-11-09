fb7f82c54c4b8e5c7d5c8ab55e77eb29f08b7d98
Harshit Patel <harshit3228@gmail.com>
2022-11-09 18:18:16 +0530

    initial comit

---
+++ routes/feedbackRoutes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const { getFeedback, setFeedback } = require("../controllers/feedbackController");
+
+router.route("/").get(getFeedback).post(setFeedback);
+
+module.exports = router;
